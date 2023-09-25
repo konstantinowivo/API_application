a24fc871f3be242bfda2652bef4c74e539b64b30
Ivo Konstantinow <ivokonstantinow@gmail.com>
2023-09-25 10:48:18 -0300

    add_consuming_API_proyect

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import {useState, useEffect} from 'react';
+import './App.css'
+import Post from './Post';
+import AddPost from './AddPost';
+
+export default function App() {
+  const [posts, setPosts] = useState([]);
+  
+  const fetchPosts = async() => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=8");
+    const data = await response.json();
+    setPosts(data);
+  }
+
+useEffect(() => {
+      fetchPosts()
+}, []);
+
+
+  const addPost = async(title, body) => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      body: JSON.stringify({
+          title: title,
+          body: body,
+          userId: Math.random().toString(36).slice(2),
+      }),
+      headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+    const data = await response.json();
+    setPosts((prevPosts) => [data, ...prevPosts])
+  };
+
+  const deletePost = async(id) => {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+      method: 'DELETE'
+    })
+      if(response.status === 200) {
+        setPosts(
+          posts.filter((post) => {
+            return post.id !== id;
+          })
+        )
+      }
+  };
+
+  return (
+    <main>
+    <h1>Consuming REST api tutorial</h1>
+      <AddPost addPost={addPost}/>
+      <section className="posts-container">
+      <h2>Posts</h2>
+        {posts.map((post) => 
+          <Post 
+            key={post.id} 
+            id={post.id}
+            title={post.title} 
+            body={post.body} 
+            deletePost={deletePost}
+          />
+        )}
+      </section>
+  </main>
+  )
+}
